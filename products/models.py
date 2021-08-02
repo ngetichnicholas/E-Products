fd3949f9897c1302031cd09be1013ef791c52329
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-08-02 11:05:58 +0300

    Add product class model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
+class Product(models.Model):
+  name = models.CharField(max_length=144)
+  seller = models.ForeignKey(User, on_delete=CASCADE)
+  description = models.TextField()
+  price = models.IntegerField()
+  quantity = models.IntegerField()
+
+
