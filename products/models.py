846fbc142ed19f09ddf36e9341c260b493088e83
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-08-02 12:18:53 +0300

    Update models

--- products/models.py
+++ products/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.deletion import CASCADE
 from django.db.models.fields.files import ImageField
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 # Create your models here.
 class Profile(models.Model):
@@ -14,6 +16,12 @@ class Profile(models.Model):
   profile_picture = ImageField(upload_to='profiles')
   location = models.CharField(max_length=144,blank=True,null=True)
 
+  @receiver(post_save,sender=User)
+  def update_profile_signal(sender, instance, created, **kwargs):
+    if created:
+      Profile.objects.create(user=instance)
+    instance.profile.save()
+  
   def __str__(self):
       return self.user.username
   
@@ -23,5 +31,8 @@ class Product(models.Model):
   description = models.TextField()
   price = models.IntegerField()
   quantity = models.IntegerField()
+
+  def __str__(self):
+      return self.name
 
 
