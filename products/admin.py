007ede9a4345a72e1c2643400ab525b1ce710012
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-09-06 09:53:43 +0300

    Update products and banner

--- products/admin.py
+++ products/admin.py
@@ -4,3 +4,4 @@ from .models import *
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(User)
+admin.site.register(Category)
--- products/models.py
+++ products/models.py
@@ -25,6 +25,9 @@ class User(AbstractUser):
 
 class Category(models.Model):
   name = models.CharField(max_length=40)
+
+  def __str__(self):
+      return self.name
   
 class Product(models.Model):
   name = models.CharField(max_length=144)
@@ -33,6 +36,8 @@ class Product(models.Model):
   price = models.IntegerField()
   quantity = models.IntegerField()
   image = models.ImageField(upload_to = 'products')
+  uploaded_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
 
   def __str__(self):
       return self.name
